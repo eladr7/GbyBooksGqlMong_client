44c6cf77e64b1a573752bef39502374d684d389e
elad <elad@twik.io>
2020-05-24 19:44:47 +0300

    My gatsby tutorial -> after preperation for production

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
-
+import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 export default function BlogPost({ data }) {
     const post = data.markdownRemark;
     return (
         <Layout>
+            <SEO title={post.frontmatter.title} description={post.excerpt} />
             <div>
                 <h1>{post.frontmatter.title}</h1>
                 <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -22,6 +23,7 @@ export const query = graphql`
       frontmatter {
         title
       }
+      excerpt
     }
   }
 `
